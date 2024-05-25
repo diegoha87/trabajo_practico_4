dc891790b82286dde443832f6904285b11f4ac17
diego <diego@DiegoHA>
2024-05-25 17:38:22 -0300

    Se crea el proyecto para la primera parte del tp 4

---
+++ tp4_AguirreDiego/src/main/java/ar/edu/unju/fi/controller/MainControllers.java
@@ -0,0 +1,28 @@
+package ar.edu.unju.fi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class MainControllers {
+	
+	@GetMapping("/contenedores")
+	public String getContainersPage() {
+		return "containers";
+	}
+	
+	@GetMapping("filascolumnas")
+	public String getFilasColumnasPage() {
+		return "filas-columnas";
+	}
+	
+	@GetMapping("/alineacion")
+	public String getAlineacionPage() {
+		return "alineacion";
+	}
+	
+	
+
+}
